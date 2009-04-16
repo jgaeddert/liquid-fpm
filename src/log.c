1641f8f090324fa6d7155b5e9f168e9856f2eb3a
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-04-16 19:10:25 +0000

    re-organizing accuracy sections

--- src/log.c
+++ src/log.c
@@ -15,6 +15,25 @@ q32_t log2_q32( q32_t _x )
         perror("error: log2_q32(), x must be > 0");
         return 0;
     }
+
+#if ACCURACY_LEVEL == 0
+    // TODO: fix this method (does not work for _x > 1.0)
+
+    // base: find most significant bit
+    int32_t b = msb_index(_x) - 1;
+
+    printf("0x%.8x > 0x%.8x\n", _x, _x^(1<<b));
+
+    // fractional portion: clear most significant bit, shift result
+    q32_t frac = (_x ^ (1<<b)) << (q32_fracbits - b);
+    
+    // scale base by data type fracbits
+    b -= q32_fracbits;
+    printf("    log2(%f) = %d + %12.10f\n", q32_fixed_to_float(_x), b, q32_fixed_to_float(frac));
+
+    // add base and fraction, negating base if necessary
+    return frac + ((b<0) ? -(abs(b)<<q32_fracbits) : (b<<q32_fracbits));
+#elif ACCURACY_LEVEL == 1
     
     // base index
     int b = msb_index(_x) - 1;
@@ -28,14 +47,7 @@ q32_t log2_q32( q32_t _x )
     // normalize by decimal position
     b -= q32_fracbits;
 
-#if ACCURACY_LEVEL == 0
-    q32_t frac = f << (q32_fracbits-8);
-#elif ACCURACY_LEVEL == 1
     q32_t frac = q32_log2_fraction_table[f];
-#else
-#   error "unknown/unsupported accuracy level"
-#endif
-
     printf("    log2(%f) = %d + %12.10f\n", q32_fixed_to_float(_x), b, q32_fixed_to_float(frac));
 
     if (b >= 0 ) {
@@ -47,6 +59,10 @@ q32_t log2_q32( q32_t _x )
     }
 
     return 0;
+#else
+#   error "unknown/unsupported accuracy level"
+#endif
+
 }
 
 #if 0
