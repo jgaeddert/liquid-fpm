77e0c826d18cf025255515e1ca944231b194688e
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-06-29 19:49:31 +0000

    fpm/log: clarifying scaling factor

--- src/log.port.c
+++ src/log.port.c
@@ -4,8 +4,12 @@
 
 #include "liquidfpm.h"
 
+// log(2) = 1/log2(e) = 0.693147180559945
+#define q32_log_2   0x0b17217f
+
 // log(x) = log2( x ) * log(2)
+//        = log2( x ) / log2(e)
 q32_t q32_log( q32_t _x )
 {
-    return q32_mul( q32_log2(_x), 0x0b17217f);
+    return q32_mul( q32_log2(_x), q32_log_2);
 }
