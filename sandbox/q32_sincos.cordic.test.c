8a221f20874544c663a359faca25fc371d70eead
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-07-01 12:04:19 +0000

    fpm: adding fixed-point cordic to sandbox

---
+++ sandbox/q32_sincos.cordic.test.c
@@ -0,0 +1,65 @@
+//
+// sin/cos using CORDIC (fixed-point)
+//
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "liquidfpm.h"
+
+int main() {
+    unsigned int n = 16;
+    double theta = q32_float_to_fixed(M_PI / 3.0);
+
+    // generate table
+    q32_t sintab[n];
+    double inv_2_n   = 1.0;
+    unsigned int i;
+    for (i=0; i<n; i++) {
+        sintab[i] = q32_float_to_fixed(atan(inv_2_n));
+        inv_2_n *= 0.5;
+    }
+
+    q32_t k_inv = q32_float_to_fixed(0.6072529350088812561694467525049282631123908521500897724);
+
+    q32_t x = k_inv;
+    q32_t y = 0;
+    q32_t z = theta;
+    q32_t v = q32_one;
+    q32_t d,tx,ty,tz;
+    printf("   n           x            y            z         -d*An\n");
+    printf("init %12.8f %12.8f %12.8f %12.8f\n",
+            q32_fixed_to_float(x),
+            q32_fixed_to_float(y),
+            q32_fixed_to_float(z),
+            0.0);
+    for (i=0; i<n; i++) {
+        d = ( z>=0.0 ) ? 1 : -1;
+
+        tx = x;
+        ty = y;
+        tz = z;
+        if (z>0) {
+            tx -= y>>i;
+            ty += x>>i;
+            tz -= sintab[i];
+        } else {
+            tx += y>>i;
+            ty -= x>>i;
+            tz += sintab[i];
+        }
+        x = tx;
+        y = ty;
+        z = tz;
+        v >>= 1;
+        printf("%4u %12.8f %12.8f %12.8f %12.8f\n",
+            i,
+            q32_fixed_to_float(x),
+            q32_fixed_to_float(y),
+            q32_fixed_to_float(z),
+            q32_fixed_to_float(q32_mul(d,sintab[i])));
+    }
+
+    return 0;
+}
