cbafd98c710c702c91d0f5dd1ad41eeb975c54cf
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-21 19:48:32 +0000

    adding log2 shift|add benchmark

---
+++ bench/q32_log2_shiftadd_benchmark.h
@@ -0,0 +1,72 @@
+//
+//
+//
+
+#ifndef __LIQUIDFPM_LOG2_SHIFTADD_BENCHMARK_H__
+#define __LIQUIDFPM_LOG2_SHIFTADD_BENCHMARK_H__
+
+#include <sys/resource.h>
+#include <math.h>
+
+#include "liquidfpm.h"
+
+#define DEBUG_LOG2_BENCH 0
+
+void precision_q32_log2_shiftadd(unsigned int _res, float * _rmse, float *_maxe)
+{
+    float e, rms_error=0.0f, max_error=0.0f;
+    float yf_min = -7.9f;
+    float yf_max = 2.9f;
+    float dyf = (yf_max-yf_min)/(_res-1);
+    float yf = yf_min;
+    float xf;
+    unsigned int i;
+    unsigned int n=16;
+
+    q32_t x, y;
+    for (i=0; i<_res; i++) {
+        xf = powf(2.0f, yf);
+
+        x = q32_float_to_fixed(xf);
+        y = q32_log2_shiftadd(x,n);
+
+        e = fabsf(yf - q32_fixed_to_float(y));
+        rms_error += e*e;
+
+        if (e > max_error || i == 0)
+            max_error = e;
+
+#if DEBUG_LOG2_BENCH
+        printf("%4u : log2(%12.8f) = %12.8f (%12.8f, e=%12.8f)\n",
+                i, xf, yf, q32_fixed_to_float(y), e);
+//        printf("e(%4u) = %12.4e;\n", i+1, e);
+#endif
+
+        yf += dyf;
+    }
+    *_rmse = sqrtf(rms_error/_res);
+    *_maxe = max_error;
+}
+
+void benchmark_q32_log2_shiftadd(struct rusage *_start,
+                    struct rusage *_finish,
+                    unsigned long int *_num_trials)
+{
+    unsigned long int i;
+    unsigned int n=16;
+    q32_t x;
+
+    // start trials
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_trials); i++) {
+        x = q32_log2_shiftadd(0x00000234,n);
+        x = q32_log2_shiftadd(0x00083018,n);
+        x = q32_log2_shiftadd(0x28591728,n);
+        x = q32_log2_shiftadd(0x02850337,n);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+
+    *_num_trials *= 4;
+}
+
+#endif // __LIQUIDFPM_LOG2_SHIFTADD_BENCHMARK_H__
