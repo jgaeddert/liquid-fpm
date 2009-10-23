0d489dcb4171c07cb93894139b60773e17673fc9
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-23 12:47:04 +0000

    atan2|cordic: fixing angle

--- src/atan.cordic.c
+++ src/atan.cordic.c
@@ -21,39 +21,23 @@ void q32_atan2_cordic(q32_t _y,
                       q32_t * _theta,
                       unsigned int _n)
 {
-#if 0
-    // For all practical purposes we are observing r = y/x so the decimal
-    // place in fixed point math can be ignored and we can treat y and x
-    // as integers so long as we don't lose resolution
-
     // negate calculated angle if _y < 0
     int negate = (_y < 0);
 
     // subtract calculated angle from pi if _x < 0
     int sub_from_pi = (_x < 0);
 
-    // subtract calculated angle from pi/2 if abs(y/x) < 1, or invert
-    // y/x to by x/y so that log2 of ratio is positive
-    int invert = 0;
-
     // take absolute value as we are only interested in absolute ratio
     // at this point.
     _y = abs(_y);
     _x = abs(_x);
 
-    // compare x, y; place larger value in y, smaller in x, thus ratio
-    // r = y/x is always greater than or equal to 1; keeps log2(y/x)
-    // positive
-    if (_y < _x) {
-        // swap _x and _y values
-        q32_t tmp = _x;
-        _x = _y;
-        _y = tmp;
-        invert = 1;
-    }
-#endif
-
     q32_atan2_cordic_base(_y,_x,_r,_theta,_n);
+    
+    if (sub_from_pi)
+        *_theta = q32_pi - *_theta;
+    if (negate)
+        *_theta = -(*_theta);
 }
 
 void q32_atan2_cordic_base(q32_t _y,
