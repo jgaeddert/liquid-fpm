b84922dbdcbd7cc66e2400cd91a5eb2be4ec0418
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-28 18:44:08 +0000

    sincos|pwpoly: computing sin,cos together

--- src/sincos.pwpolyfit.c
+++ src/sincos.pwpolyfit.c
@@ -31,10 +31,10 @@ Q(_t) Q(_sine_table_pwpolyfit)[16][3] = {
     {0xec49dce1, 0x276c7700, 0xfc49ac31}
 };
 
-Q(_t) Q(_polyval_p2)(Q(_t) _x,
-                     Q(_t) _c0,
-                     Q(_t) _c1,
-                     Q(_t) _c2)
+Q(_t) Q(_sincos_polyval_p2)(Q(_t) _x,
+                            Q(_t) _c0,
+                            Q(_t) _c1,
+                            Q(_t) _c2)
 {
     return _c0 + Q(_mul)(_c1,_x) + Q(_mul)(_c2,Q(_mul)(_x,_x));
 }
@@ -65,105 +65,111 @@ void Q(_angle_quadrant_index)(Q(_t) _theta,
     //   2. Mask with 0x000F (15), the maximum index for lookup table
     *_index = (_theta >> 25) & 0x000F;
 
+    // TODO : set appropriate shift amount
+    // x is in [0,1)
     *_x = (_theta >> 1) & 0x0fffffff;
 }
 
-Q(_t) Q(_sin)( Q(_t) _theta )
+void Q(_sincos_pwpoly)(Q(_t) _theta,
+                       Q(_t) * _sin,
+                       Q(_t) * _cos)
 {
     unsigned int quadrant;
-    unsigned int index;
-    Q(_t) x;
+    unsigned int sin_index;
+    unsigned int cos_index;
+    Q(_t) xs;
+    Q(_t) xc;
+    
+    // polynomial coefficients
+    Q(_t) c2;
+    Q(_t) c1;
+    Q(_t) c0;
 
     // Extract 2-bit quadrant and 8-bit phase index
     // Q1 | Q0
     // ---+---
     // Q2 | Q3
-    Q(_angle_quadrant_index)(_theta, &quadrant, &index, &x);
+    Q(_angle_quadrant_index)(_theta, &quadrant, &sin_index, &xs);
+    cos_index = sin_index;
+    xc = xs;
 
 #if DEBUG_SINCOS_PWPOLY
     printf("    theta : %12.8f (0x%.8x > %12.8f)\n", Q(_angle_fixed_to_float)(_theta), _theta, Q(_fixed_to_float)(_theta));
     printf("    quadrant : %u\n", quadrant);
-    printf("    index    : %u\n", index);
+    printf("    index    : %u\n", sin_index);
     printf("    x        : %f\n", Q(_fixed_to_float)(x));
 #endif
 
     // sine: for quadrants Q1 & Q3 phase is inverted
     if (quadrant & 0x01) {
-        x = (1<<Q(_fracbits)) - x;
-        index = 15 - index;
+        xs = (1<<Q(_fracbits)) - xs;
+        sin_index = 15 - sin_index;
 #if DEBUG_SINCOS_PWPOLY
-        printf("    x        : %f (phase inversion)\n", Q(_fixed_to_float)(x));
-        printf("    index    : %u (phase inversion)\n", index);
+        printf("  sine:\n");
+        printf("    x        : %f (phase inversion)\n", Q(_fixed_to_float)(xs));
+        printf("    index    : %u (phase inversion)\n", sin_index);
 #endif
     }
 
-    Q(_t) c2 = Q(_sine_table_pwpolyfit)[index][0];
-    Q(_t) c1 = Q(_sine_table_pwpolyfit)[index][1];
-    Q(_t) c0 = Q(_sine_table_pwpolyfit)[index][2];
+    // compute sine
+    c2 = Q(_sine_table_pwpolyfit)[sin_index][0];
+    c1 = Q(_sine_table_pwpolyfit)[sin_index][1];
+    c0 = Q(_sine_table_pwpolyfit)[sin_index][2];
 #if DEBUG_SINCOS_PWPOLY
+    printf("  sine:\n");
     printf("    c0 : %12.8f\n", Q(_fixed_to_float)(c0));
     printf("    c1 : %12.8f\n", Q(_fixed_to_float)(c1));
     printf("    c2 : %12.8f\n", Q(_fixed_to_float)(c2));
 #endif
-
     // sine: for quadrants Q2 & Q3 value is negated
-    Q(_t) retval = Q(_polyval_p2)(x,c0,c1,c2);
+    Q(_t) s = Q(_sincos_polyval_p2)(xs,c0,c1,c2);
     if (quadrant & 0x02)
-        retval = -retval;
-
-    return retval;
-}
-
-Q(_t) Q(_cos)( Q(_t) _theta )
-{
-    unsigned int quadrant;
-    unsigned int index;
-    Q(_t) x;
-
-    // Extract 2-bit quadrant and 8-bit phase index
-    // Q1 | Q0
-    // ---+---
-    // Q2 | Q3
-    Q(_angle_quadrant_index)(_theta, &quadrant, &index, &x);
-
-#if DEBUG_SINCOS_PWPOLY
-    printf("    theta : %12.8f (0x%.8x > %12.8f)\n", Q(_angle_fixed_to_float)(_theta), _theta, Q(_fixed_to_float)(_theta));
-    printf("    quadrant : %u\n", quadrant);
-    printf("    index    : %u\n", index);
-    printf("    x        : %f\n", Q(_fixed_to_float)(x));
-#endif
+        s = -s;
 
     // cosine: for quadrants Q0 & Q2 phase is inverted
-    if ( quadrant==0 || quadrant==2 ) {
-        x = (1<<Q(_fracbits)) - x;
-        index = 15 - index;
+    if (quadrant == 0 || quadrant == 2) {
+        xc = (1<<Q(_fracbits)) - xc;
+        cos_index = 15 - cos_index;
 #if DEBUG_SINCOS_PWPOLY
-        printf("    x        : %f (phase inversion)\n", Q(_fixed_to_float)(x));
-        printf("    index    : %u (phase inversion)\n", index);
+        printf("  cosine:\n");
+        printf("    x        : %f (phase inversion)\n", Q(_fixed_to_float)(xc));
+        printf("    index    : %u (phase inversion)\n", cos_index);
 #endif
     }
 
-    Q(_t) c2 = Q(_sine_table_pwpolyfit)[index][0];
-    Q(_t) c1 = Q(_sine_table_pwpolyfit)[index][1];
-    Q(_t) c0 = Q(_sine_table_pwpolyfit)[index][2];
+    // compute cosine
+    c2 = Q(_sine_table_pwpolyfit)[cos_index][0];
+    c1 = Q(_sine_table_pwpolyfit)[cos_index][1];
+    c0 = Q(_sine_table_pwpolyfit)[cos_index][2];
 #if DEBUG_SINCOS_PWPOLY
+    printf("  cosine:\n");
     printf("    c0 : %12.8f\n", Q(_fixed_to_float)(c0));
     printf("    c1 : %12.8f\n", Q(_fixed_to_float)(c1));
     printf("    c2 : %12.8f\n", Q(_fixed_to_float)(c2));
 #endif
-
     // cosine: for quadrants Q1 & Q2 value is negated
-    Q(_t) retval = Q(_polyval_p2)(x,c0,c1,c2);
+    Q(_t) c = Q(_sincos_polyval_p2)(xc,c0,c1,c2);
     if ( quadrant==1 || quadrant==2 )
-        retval = -retval;
+        c = -c;
 
-    return retval;
-
+    *_sin = s;
+    *_cos = c;
 }
 
-void Q(_sincos)( Q(_t) _theta, Q(_t) *_sin, Q(_t) *_cos )
+
+Q(_t) Q(_sin)(Q(_t) _theta)
 {
-    // TODO: implement more efficient method
-    *_sin = Q(_sin)(_theta);
-    *_cos = Q(_cos)(_theta);
+    Q(_t) s;
+    Q(_t) c;
+    Q(_sincos_pwpoly)(_theta, &s, &c);
+    return s;
 }
+
+Q(_t) Q(_cos)(Q(_t) _theta)
+{
+    Q(_t) s;
+    Q(_t) c;
+    Q(_sincos_pwpoly)(_theta, &s, &c);
+    return c;
+}
+
