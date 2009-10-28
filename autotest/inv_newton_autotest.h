d976e46982216099a39ec3ddb6e351fe7c3437af
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-28 21:28:12 +0000

    adding legalese junk

--- autotest/inv_newton_autotest.h
+++ autotest/inv_newton_autotest.h
@@ -1,6 +1,22 @@
-//
-// 
-//
+/*
+ * Copyright (c) 2008, 2009 Joseph Gaeddert
+ * Copyright (c) 2008, 2009 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid-fpm.
+ *
+ * liquid-fpm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid-fpm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid-fpm.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LIQUIDFPM_INV_NEWTON_AUTOTEST_H
 #define LIQUIDFPM_INV_NEWTON_AUTOTEST_H
