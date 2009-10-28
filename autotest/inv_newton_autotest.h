2f62c5822cdc8d22905843714a522110902d0b24
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-28 21:26:07 +0000

    cleaning up default autotest

--- autotest/inv_newton_autotest.h
+++ autotest/inv_newton_autotest.h
@@ -15,19 +15,22 @@
 
 void inv_newton_autotest()
 {
-    printf("testing inv() [Newton]...\n");
-    unsigned int num_steps = 32;
-    float xmin = 1.0f / 7.99f;
-    float xmax = 7.99f;
-    float dx = (xmax - xmin)/((float)(num_steps-1));
-    float tol = 0.1f;
+    if (_autotest_verbose)
+        printf("testing inv() [Newton]...\n");
 
+    unsigned int n=32;  // precision
+    unsigned int num_steps = 32;
+    float xmin = 1.0f / Q(_fixed_to_float)(Q(_max));
+    float xmax = Q(_fixed_to_float)(Q(_max));
+    float dx = (xmax - xmin)/((float)(num_steps-1));
+    float tol = 1e-5f;
+
+    // testing variables
     float xf;
     float yf;
     q32_t x;
     q32_t y;
     float ytest;
-    unsigned int n=32;
 
     unsigned int i;
     xf = xmin;
@@ -43,9 +46,12 @@ void inv_newton_autotest()
 
         CONTEND_DELTA(yf,ytest,tol);
 
-        printf("inv(%12.8f) = %12.8f (%12.8f)\n", xf,
-                                                  q32_fixed_to_float(y),
-                                                  yf);
+        if (_autotest_verbose) {
+            printf("inv(%12.8f) = %12.8f (%12.8f)\n", xf,
+                                                      q32_fixed_to_float(y),
+                                                      yf);
+        }
+
         // increment input parameter
         xf += dx;
     }
