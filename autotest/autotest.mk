bd2ad825419596e8095e9abe5e78deea3731c363
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-28 21:42:20 +0000

    placing autotest headers in autotest subdirectory; eases adding autotests to project (no need to re-run configure)

---
+++ autotest/autotest.mk
@@ -0,0 +1,11 @@
+#
+# autotest.mk
+#
+# Autotest makefile, included by root makefile.  Defines
+# a list of headers
+#
+
+autotest_headers =				\
+	autotest/inv_newton_autotest.h		\
+	autotest/sqrt_newton_autotest.h
+
