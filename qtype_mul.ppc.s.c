2b2de4fe0c1a981177049d5b09292d655b67bf6f
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-29 18:06:04 +0000

    assembly source generator: adding option for different architectures

--- qtype_mul.ppc.s.c
+++ qtype_mul.ppc.s.c
@@ -10,10 +10,138 @@
 void usage(void)
 {
     printf("  u/h   :   print this help file\n");
+    printf("    a   :   target architecture: ppc, x86, intelmac\n");
     printf("    n   :   name (e.g. q32b16)\n");
     printf("    i   :   intbits (including sign bit)\n");
     printf("    f   :   fracbits\n");
     printf("    o   :   output filename [default: standard output]\n");
+}
+
+//typedef void (*qtype_mul_func)(FILE*, const char*, unsigned int, unsigned int);
+//qtype_mul_func qtype_mul_ppc;
+
+void qtype_mul_ppc(FILE * _fid,
+                   const char * _name,
+                   unsigned int _intbits,
+                   unsigned int _fracbits);
+
+void qtype_mul_x86(FILE * _fid,
+                   const char * _name,
+                   unsigned int _intbits,
+                   unsigned int _fracbits);
+
+void qtype_mul_intelmac(FILE * _fid,
+                        const char * _name,
+                        unsigned int _intbits,
+                        unsigned int _fracbits);
+
+int main(int argc, char *argv[])
+{
+    // data type name
+    char typename[64];
+    int  typename_set=0;
+
+    // target architecture (function pointer to
+    // source code generator)
+    void (*func)(FILE *, const char *, unsigned int, unsigned int) = NULL;
+    int arch_set=0;
+
+    // number of integer bits
+    unsigned int intbits=0;
+    int intbits_set=0;
+
+    // number of fractional bits
+    unsigned int fracbits=0;
+    int fracbits_set=0;
+
+    // output file
+    char outputfile[64];
+    int outputfile_set=0;
+
+    int dopt;
+    while ((dopt = getopt(argc,argv,"ua:n:i:f:o:")) != EOF) {
+        switch (dopt) {
+        case 'u':
+        case 'h':
+            usage(); return 0;
+        case 'a':
+            if (strcmp(optarg,"ppc")==0) {
+                // Building for PowerPC
+                func = &qtype_mul_ppc;
+            /*
+            } else if (strcmp(optarg,"x86")==0) {
+                // Building for x86
+            } else if (strcmp(optarg,"intelmac")==0) {
+                // Building for Intel mac
+            */
+            } else {
+                fprintf(stderr,"error: unknown architecture: %s\n", optarg);
+                exit(1);
+            }
+            arch_set = 1;
+            break;
+        case 'n':
+            if (strlen(optarg) > 64) {
+                printf("error: typename too long\n");
+                return -1;
+            }
+            strcpy(typename,optarg); 
+            typename_set = 1;
+            break;
+        case 'i':
+            intbits = atoi(optarg);
+            intbits_set = 1;
+            break;
+        case 'f':
+            fracbits = atoi(optarg);
+            fracbits_set = 1;
+            break;
+        case 'o':
+            if (strlen(optarg) > 64) {
+                printf("error: output filename too long\n");
+                return -1;
+            }
+            strcpy(outputfile,optarg); 
+            outputfile_set = 1;
+            break;
+        default:
+            usage();
+            return -1;
+        }
+    }
+
+    if ( !arch_set || !typename_set || !intbits_set  || !fracbits_set ) {
+        fprintf(stderr,"error: must specify architecture, typename, intbits, and fracbits\n");
+        usage();
+        return -1;
+    } else if ( intbits < 1 ) {
+        fprintf(stderr,"error: intbits must be at least 1 (sign bit)\n");
+        return -1;
+    } else if ( (intbits + fracbits) != 32 ) {
+        fprintf(stderr,"error: intbits (%u) + fracbits (%u) must equal 32\n",intbits,fracbits);
+        return -1;
+    }
+
+    // open output file
+    FILE * fid;
+    if (outputfile_set) {
+        fid = fopen(outputfile,"w");
+        if (fid == NULL) {
+            printf("error: could not open %s for writing\n",outputfile);
+            return -1;
+        }
+    } else {
+        fid = stdout;
+    }
+
+    // run the assembly source code generator
+    func(fid,typename,intbits,fracbits);
+
+    // close the file (if not printing to stdout)
+    if (outputfile_set)
+        fclose(fid);
+
+    return 0;
 }
 
 void qtype_mul_ppc(FILE * _fid,
@@ -45,93 +173,5 @@ void qtype_mul_ppc(FILE * _fid,
     fprintf(_fid,"\n");
     fprintf(_fid,"    blr                 ; return from routine\n");
     fprintf(_fid,"    .subsections_via_symbols\n");
-}
-
-int main(int argc, char *argv[])
-{
-    // data type name
-    char typename[64];
-    int  typename_set=0;
-
-    // number of integer bits
-    unsigned int intbits=0;
-    int intbits_set=0;
-
-    // number of fractional bits
-    unsigned int fracbits=0;
-    int fracbits_set=0;
-
-    // output file
-    char outputfile[64];
-    int outputfile_set=0;
-
-    int dopt;
-    while ((dopt = getopt(argc,argv,"un:i:f:o:")) != EOF) {
-        switch (dopt) {
-        case 'u':
-        case 'h':
-            usage(); return 0;
-        case 'n':
-            if (strlen(optarg) > 64) {
-                printf("error: typename too long\n");
-                return -1;
-            }
-            strcpy(typename,optarg); 
-            typename_set = 1;
-            break;
-        case 'i':
-            intbits = atoi(optarg);
-            intbits_set = 1;
-            break;
-        case 'f':
-            fracbits = atoi(optarg);
-            fracbits_set = 1;
-            break;
-        case 'o':
-            if (strlen(optarg) > 64) {
-                printf("error: output filename too long\n");
-                return -1;
-            }
-            strcpy(outputfile,optarg); 
-            outputfile_set = 1;
-            break;
-        default:
-            usage();
-            return -1;
-        }
-    }
-
-    if ( !typename_set || !intbits_set  || !fracbits_set ) {
-        printf("error: must specify typename, intbits, and fracbits\n");
-        usage();
-        return -1;
-    } else if ( intbits < 1 ) {
-        printf("error: intbits must be at least 1 (sign bit)\n");
-        return -1;
-    } else if ( (intbits + fracbits) != 32 ) {
-        printf("error: intbits (%u) + fracbits (%u) must equal 32\n",intbits,fracbits);
-        return -1;
-    }
-
-    // open output file
-    FILE * fid;
-    if (outputfile_set) {
-        fid = fopen(outputfile,"w");
-        if (fid == NULL) {
-            printf("error: could not open %s for writing\n",outputfile);
-            return -1;
-        }
-    } else {
-        fid = stdout;
-    }
-
-    // run the assembly source code generator
-    qtype_mul_ppc(fid,typename,intbits,fracbits);
-
-    // close the file (if not printing to stdout)
-    if (outputfile_set)
-        fclose(fid);
-
-    return 0;
 }
 
