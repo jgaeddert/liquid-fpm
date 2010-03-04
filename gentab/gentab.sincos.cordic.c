7bae6845bfb993e3b02749c67d7312276f0f4f32
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-03-04 23:41:28 +0000

    gentab|sincos: adding command-line options

--- gentab/gentab.sincos.cordic.c
+++ gentab/gentab.sincos.cordic.c
@@ -20,48 +20,95 @@
  */
 
 //
-// Table generation for CORDIC
+// Table generation for sin|cos CORDIC
 //
 
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <getopt.h>
 
-#include "liquidfpm.h"
+#include "liquidfpm.internal.h"
 
-int main() {
+void usage(void)
+{
+    printf("gentab.sincos.cordic usage:\n");
+    printf("  u/h   :   print this help file\n");
+    printf("    n   :   name (e.g. q32b16)\n");
+    printf("    i   :   intbits (including sign bit)\n");
+    printf("    f   :   fracbits\n");
+    //printf("    o   :   output filename [default: standard output]\n");
+}
+
+int main(int argc, char*argv[]) {
+    // options
+    FILE * fid = stdout;
+    char qtype[64] = "q32";
+    unsigned int intbits = 7;
+    unsigned int fracbits = 25;
+
+    // read options
+    int dopt;
+    while ((dopt = getopt(argc,argv,"uhn:i:f:")) != EOF) {
+        switch (dopt) {
+        case 'u':
+        case 'h':   usage();                    return 0;
+        case 'n':   strncpy(qtype,optarg,64);   break;
+        case 'i':   intbits = atoi(optarg);     break;
+        case 'f':   fracbits = atoi(optarg);    break;
+        default:
+            fprintf(stderr,"error: %s, unknown option\n", argv[0]);
+            usage();
+            return 1;
+        }
+    }
+
+    // validate length
+    unsigned int totalbits = intbits + fracbits;
+    if (totalbits != 8 && totalbits != 16 && totalbits != 32) {
+        fprintf(stderr,"error: %s, invalid total bits (%u), must be 8,16,32\n", argv[0], totalbits);
+        exit(-1);
+    }
+
     unsigned int n = 32;
 
+    fprintf(fid,"// auto-generated file : do not edit\n");
+    fprintf(fid,"\n");
+    fprintf(fid,"#include \"liquidfpm.internal.h\"\n");
+    fprintf(fid,"\n");
+
+    // 1/K scaling factor
+    float k_inv = 0.60725293500888125616;
+
+    fprintf(fid,"// scaling factor 1/K:  K = prod(k=0,infty){ sqrt(1 + 2^(-2k)) }\n");
+    fprintf(fid,"const %s_t %s_cordic_k_inv = 0x%.8x;\n\n", qtype,qtype,
+            qtype_float_to_fixed(k_inv,intbits,fracbits));
+
     // generate table
-    q32_t sintab[n];
     double inv_2_n   = 1.0;
     double Ak;
     unsigned int i;
+    float qtype_angle_scalar = M_PI / (float)(1<<(intbits-2));
+    fprintf(fid,"// cordic coefficients: A[k] = arctan(2^-k)\n");
+    fprintf(fid,"const %s_t %s_cordic_Ak_tab[%u] = {\n", qtype,qtype,n);
     for (i=0; i<n; i++) {
         // precompute Ak, normalizing by angular scaling factor. This
         // is necessary by the nature of how angles are stored in
         // fixed-point decimal:
         //    2*pi  :   0x7fffffff (the largest positive number)
         //   -2*pi  :   0xffffffff (the largest negative number)
-        Ak = atanf(inv_2_n) / q32_angle_scalar;
-        sintab[i] = q32_float_to_fixed(Ak);
+        Ak = atanf(inv_2_n) / qtype_angle_scalar;
+
+        // write output
+        fprintf(fid,"    0x%.8x%s",
+                qtype_float_to_fixed(Ak,intbits,fracbits),
+                (i<n-1) ? ",\n" : "};\n\n");
+
+        // update 1 / 2^n
         inv_2_n *= 0.5;
     }
 
-    q32_t k_inv = q32_float_to_fixed(0.6072529350088812561694467525049282631123908521500897724);
-
-    printf("// auto-generated file : do not edit\n");
-    printf("\n");
-    printf("#include \"liquidfpm.internal.h\"\n");
-    printf("\n");
-
-    printf("// cordic coefficients: A[k] = arctan(2^-k)\n");
-    printf("const q32_t q32_cordic_Ak_tab[%u] = {\n", n);
-    for (i=0; i<n; i++)
-        printf("    0x%.8x%s",sintab[i], (i<n-1) ? ",\n" : "};\n\n");
-
-    printf("// scaling factor 1/K:  K = prod(k=0,infty){ sqrt(1 + 2^(-2k)) }\n");
-    printf("const q32_t q32_cordic_k_inv = 0x%.8x;\n\n", k_inv);
-
     return 0;
 }
+
