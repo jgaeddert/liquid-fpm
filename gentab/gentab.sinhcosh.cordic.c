6505bf68e4bacff0f73ab44eaba82ba6d5af404f
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-03-05 00:00:31 +0000

    gentab|sinhcosh|cordic: adding command-line options

--- gentab/gentab.sinhcosh.cordic.c
+++ gentab/gentab.sinhcosh.cordic.c
@@ -26,35 +26,58 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <getopt.h>
 
-#include "liquidfpm.h"
+#include "liquidfpm.internal.h"
 
-int main() {
+void usage(void)
+{
+    printf("gentab.sinhcosh.cordic usage:\n");
+    printf("  u/h   :   print this help file\n");
+    printf("    n   :   name (e.g. q32b16)\n");
+    printf("    i   :   intbits (including sign bit)\n");
+    printf("    f   :   fracbits\n");
+    //printf("    o   :   output filename [default: standard output]\n");
+}
+
+int main(int argc, char*argv[]) {
+    // options
+    FILE * fid = stdout;
+    char qtype[64] = "q32";
+    unsigned int intbits = 7;
+    unsigned int fracbits = 25;
     unsigned int n = 32;
 
-    // generate table
-    q32_t sinhtab[n];
-    double inv_2_n   = 1.0;
-    double Ak;
-    unsigned int k;
-    for (k=0; k<n; k++) {
-        // precompute Ak
-        Ak = atanhf(inv_2_n);
-        sinhtab[k] = q32_float_to_fixed(Ak);
-        inv_2_n *= 0.5;
+    // read options
+    int dopt;
+    while ((dopt = getopt(argc,argv,"uhn:i:f:")) != EOF) {
+        switch (dopt) {
+        case 'u':
+        case 'h':   usage();                    return 0;
+        case 'n':   strncpy(qtype,optarg,64);   break;
+        case 'i':   intbits = atoi(optarg);     break;
+        case 'f':   fracbits = atoi(optarg);    break;
+        default:
+            fprintf(stderr,"error: %s, unknown option\n", argv[0]);
+            usage();
+            return 1;
+        }
     }
 
-    q32_t kp_inv = q32_float_to_fixed(1.2074970677630721288777210113109158368127832217698134);
+    // validate length
+    unsigned int totalbits = intbits + fracbits;
+    if (totalbits != 8 && totalbits != 16 && totalbits != 32) {
+        fprintf(stderr,"error: %s, invalid total bits (%u), must be 8,16,32\n", argv[0], totalbits);
+        exit(-1);
+    }
 
     printf("// auto-generated file : do not edit\n");
     printf("\n");
     printf("#include \"liquidfpm.internal.h\"\n");
     printf("\n");
 
-    printf("// sinh|cosh cordic coefficients: A[k] = arctanh(2^-k)\n");
-    printf("const q32_t q32_sinhcosh_cordic_Ak_tab[%u] = {\n", n);
-    for (k=0; k<n; k++)
-        printf("    0x%.8x%s",sinhtab[k], (k<n-1) ? ",\n" : "};\n\n");
+    float kp_inv = 1.207497067763072128878;
 
     printf("// scaling factor 1/Kp:  \n");
     printf("//   Kp = prod(k=0,infty){ sqrt(1 - 2^(-2*k)) } *\n");
@@ -62,7 +85,28 @@ int main() {
     printf("// where\n");
     printf("//   i_0     = 4,\n");
     printf("//   i_(k+1) = 3*i_k + 1\n");
-    printf("const q32_t q32_sinhcosh_cordic_kp_inv = 0x%.8x;\n\n", kp_inv);
+    printf("const %s_t %s_sinhcosh_cordic_kp_inv = 0x%.8x;\n\n", 
+            qtype,qtype,
+            qtype_float_to_fixed(kp_inv,intbits,fracbits));
+
+    // generate table
+    printf("// sinh|cosh cordic coefficients: A[k] = arctanh(2^-k)\n");
+    printf("const %s_t %s_sinhcosh_cordic_Ak_tab[%u] = {\n", qtype,qtype,n);
+    double inv_2_n   = 1.0;
+    double Ak;
+    unsigned int k;
+    for (k=0; k<n; k++) {
+        // precompute Ak (first value is ignored in algorithm regardless)
+        Ak = k == 0 ? 0 : atanhf(inv_2_n);
+
+        // write outpt value
+        printf("    0x%.8x%s",
+                qtype_float_to_fixed(Ak,intbits,fracbits),
+                (k<n-1) ? ",\n" : "};\n\n");
+
+        // update 1 / 2^n
+        inv_2_n *= 0.5;
+    }
 
     return 0;
 }
